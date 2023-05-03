ff72b98a8dc7ad9bda970f3a5b6f67d6296cfb84
Deshpetko Maksym <mdeshpetko@gmail.com>
2023-05-03 04:36:12 +0300

    changed code by putting enum in singletone class

--- patterns/src/main/java/pattern/creation/singelton/Singleton.java
+++ patterns/src/main/java/pattern/creation/singelton/Singleton.java
@@ -18,24 +18,19 @@ public final class Singleton {
     }
 
     private static Singleton instance;
-    private static  Database  lock = Database.MYSQL;
+    private static Object lock = new Object();
 
     private Singleton() {
     }
 
-    public static Singleton getInstance(Database database) {
+    public static Singleton getInstance() {
         if (instance == null) {
             synchronized (lock) {
-                lock = database;
                 if (instance == null) {
                     instance = new Singleton();
                 }
             }
         }
         return instance;
-    }
-
-    public static Database getLock(){
-        return lock;
     }
 }
