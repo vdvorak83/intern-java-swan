575c714b29f1367eed3d019961b4f7f5185b1e69
Deshpetko Maksym <mdeshpetko@gmail.com>
2023-05-03 04:23:40 +0300

    changed code by putting enum in singletone class

--- patterns/src/main/java/pattern/creation/singelton/Singleton.java
+++ patterns/src/main/java/pattern/creation/singelton/Singleton.java
@@ -1,6 +1,22 @@
 package pattern.creation.singelton;
 
 public final class Singleton {
+    public enum Database {
+        MYSQL("MySQL Database"),
+        POSTGRESQL("PostgreSQL Database"),
+        ORACLE("Oracle Database");
+
+        private String description;
+
+        Database(String description) {
+            this.description = description;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+    }
+
     private static Singleton instance;
     private static Object lock = new Object();
 
--- patterns/src/main/java/pattern/creation/singelton/SingletonTestCase.java
+++ patterns/src/main/java/pattern/creation/singelton/SingletonTestCase.java
@@ -4,19 +4,19 @@ public class SingletonTestCase {
     public static void main(String[] args) {
         Thread t1 = new Thread(() -> {
             Singleton singleton = Singleton.getInstance();
-            Database database = Database.MYSQL;
+            Singleton.Database database = Singleton.Database.MYSQL;
             System.out.println("Thread 1: " + singleton + " connected to " + database.getDescription());
         });
 
         Thread t2 = new Thread(() -> {
             Singleton singleton = Singleton.getInstance();
-            Database database = Database.POSTGRESQL;
+            Singleton.Database database = Singleton.Database.POSTGRESQL;
             System.out.println("Thread 2: " + singleton + " connected to " + database.getDescription());
         });
 
         Thread t3 = new Thread(() -> {
             Singleton singleton = Singleton.getInstance();
-            Database database = Database.ORACLE;
+            Singleton.Database database = Singleton.Database.ORACLE;
             System.out.println("Thread 3: " + singleton + " connected to " + database.getDescription());
         });
 
