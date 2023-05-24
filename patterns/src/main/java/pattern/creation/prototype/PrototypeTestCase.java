245d99c64eb0d63a83a5d776ecfbdf1ab6a8925e
Maksym Deshpetko <98956631+phx67@users.noreply.github.com>
2023-05-24 08:34:04 +0300

    prototype (#8)
    created a task with prototype realization

--- patterns/src/main/java/pattern/creation/prototype/PrototypeTestCase.java
+++ patterns/src/main/java/pattern/creation/prototype/PrototypeTestCase.java
@@ -1,7 +1,16 @@
 package pattern.creation.prototype;
 
 public class PrototypeTestCase {
-    public static void main(String[] args) {
-        System.out.println("Prototype");
+    public static void main(String[] args)  {
+        Storage.addPrototype("myClass", new Forclone("original"));
+
+        Prototype myClassCopy = Storage.getPrototype("myClass");
+        System.out.println("Original name: " + ((Forclone) Storage.getPrototype("myClass")).getName());
+        System.out.println("Copy name: " + ((Forclone) myClassCopy).getName());
+
+        ((Forclone) myClassCopy).setName("new");
+
+        System.out.println("Original name after change: " + ((Forclone) Storage.getPrototype("myClass")).getName());
+        System.out.println("Copy name after change: " + ((Forclone) myClassCopy).getName());
     }
 }
