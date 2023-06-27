c7611f1d2cc86e1ab8e75c4182f8663161a353c5
Maksym Deshpetko <98956631+phx67@users.noreply.github.com>
2023-06-27 16:38:43 +0300

    decorator (#9)
    my completed task with pattern decorator

---
+++ patterns/src/main/java/pattern/creation/decorator/Coffee.java
@@ -0,0 +1,6 @@
+package pattern.creation.decorator;
+
+public interface Coffee {
+    double getCost();
+    String getDescription();
+}
---
+++ patterns/src/main/java/pattern/creation/decorator/MilkDecorator.java
@@ -0,0 +1,17 @@
+package pattern.creation.decorator;
+
+class MilkDecorator extends CoffeeDecorator {
+    public MilkDecorator(Coffee coffee) {
+        super(coffee);
+    }
+
+    @Override
+    public double getCost() {
+        return super.getCost() + 0.5;
+    }
+
+    @Override
+    public String getDescription() {
+        return super.getDescription() + ", with milk";
+    }
+}
