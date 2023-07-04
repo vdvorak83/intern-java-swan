1428e5f18a4bd74911d31b6168809dd3fb188efc
Maksym Deshpetko <98956631+phx67@users.noreply.github.com>
2023-07-04 13:00:43 +0300

    i created program with realization pattern singleton (#5)
    * i created program with realization pattern singleton

--- patterns/src/main/java/pattern/creation/singelton/Singleton.java
+++ patterns/src/main/java/pattern/creation/singelton/Singleton.java
@@ -1,5 +1,36 @@
 package pattern.creation.singelton;
 
-public  class Singleton {
+public final class Singleton {
+    public enum Database {
+        MYSQL("MySQL Database"),
+        POSTGRESQL("PostgreSQL Database"),
+        ORACLE("Oracle Database");
 
+        private String description;
+
+        Database(String description) {
+            this.description = description;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+    }
+
+    private static Singleton instance;
+    private static Object lock = new Object();
+
+    private Singleton() {
+    }
+
+    public static Singleton getInstance() {
+        if (instance == null) {
+            synchronized (lock) {
+                if (instance == null) {
+                    instance = new Singleton();
+                }
+            }
+        }
+        return instance;
+    }
 }
--- patterns/src/main/java/pattern/creation/singelton/SingletonTestCase.java
+++ patterns/src/main/java/pattern/creation/singelton/SingletonTestCase.java
@@ -2,7 +2,26 @@ package pattern.creation.singelton;
 
 public class SingletonTestCase {
     public static void main(String[] args) {
-        System.out.println("Singelton test!");
-    }
+        Thread t1 = new Thread(() -> {
+            Singleton singleton = Singleton.getInstance();
+            Singleton.Database database = Singleton.Database.MYSQL;
+            System.out.println("Thread 1: " + singleton + " connected to " + database.getDescription());
+        });
 
+        Thread t2 = new Thread(() -> {
+            Singleton singleton = Singleton.getInstance();
+            Singleton.Database database = Singleton.Database.POSTGRESQL;
+            System.out.println("Thread 2: " + singleton + " connected to " + database.getDescription());
+        });
+
+        Thread t3 = new Thread(() -> {
+            Singleton singleton = Singleton.getInstance();
+            Singleton.Database database = Singleton.Database.ORACLE;
+            System.out.println("Thread 3: " + singleton + " connected to " + database.getDescription());
+        });
+
+        t1.start();
+        t2.start();
+        t3.start();
+    }
 }
